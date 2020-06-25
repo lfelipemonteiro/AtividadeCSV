bc762bcf8313233ce10815e5484eb3b9c4f317d0
dukra <dukra@DESKTOP-N3M25A5>
2020-06-25 17:54:10 -0300

    Atividade CSV para aula de Sistema Operacionais

--- src/view/Principal.java
+++ src/view/Principal.java
@@ -1,5 +1,17 @@
 package view;
 
-public class Principal {
+import java.io.IOException;
 
+import controller.ControllerArquivo;
+
+public class Principal {
+	public static void main(String[] args) {
+		ControllerArquivo ca = new ControllerArquivo();
+		try {
+			ca.readFile();
+		} catch (IOException e) {
+			// TODO Auto-generated catch block
+			e.printStackTrace();
+		};
+	}
 }
